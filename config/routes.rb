ca2b070f15f0bb45f672b86d232b8eb1d960e81c
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-23 11:30:35 -0600

    TEST - user can add miles run manually

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,13 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
 
   resources :users, only: [:create, :new, :destroy]
-  
+
   get '/dashboard', to: 'dashboard#show'
 
   get :feed, to: 'feed#index', as: 'feed'
   get :buddies, to: 'buddies#index', as: 'buddies'
   get :hangout, to: 'hangout#index', as: 'hangout'
-  # get :hangout, to: 'hangout#show'
+  resources :record
   resources :goals, only: [:index, :show, :new, :update]
   resources :plans, only: [:index, :show]
 
--- spec/features/user/user_can_add_miles_run_spec.rb
+++ spec/features/user/user_can_add_miles_run_spec.rb
@@ -11,5 +11,12 @@ describe 'User' do
     fill_in 'Workout name', with: 'first run'
     fill_in 'Date', with: Date.today
     fill_in 'Distance', with: '5'
+
+    click_on 'Save'
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content('first run')
+    expect(page).to have_content(Date.today)
+    expect(page).to have_content('5 miles')
   end
 end
