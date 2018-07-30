9b8982cebb9d1e3f0125e32c430a1f91b759bf66
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-29 21:21:12 -0600

    Added a Plan PORO

--- app/controllers/user_plans_controller.rb
+++ app/controllers/user_plans_controller.rb
@@ -1,6 +1,8 @@
 class UserPlansController < ApplicationController
   def new
-
+    # binding.pry
+    # @plan = PlanCSVReader.new(params[:plan_id])
+    @plan = Plan.new
   end
 
   def create
@@ -8,11 +10,20 @@ class UserPlansController < ApplicationController
   end
 
   def index
-    plan = PlanCSVReader.new(params)
-    # current_user.plans
+    current_user.plans
   end
 
   def show
     user_plans.find_by(params[:id])
+  end
+end
+
+class Plan
+  def initialize
+  end
+
+  def self.find_plan(name)
+    binding.pry
+
   end
 end
