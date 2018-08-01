b890849f915a05b15c3b87397ea0f33170c8d6a9
Sabrina Robinson <sabrinarob@gmail.com>
2018-07-31 22:40:31 -0600

    Working on reminder PORO

--- app/models/remind_users.rb
+++ app/models/remind_users.rb
@@ -1,5 +1,20 @@
 class RemindUsers
   def initialize(time)
     @time = time
+    @today = Date.today
+    @text_client = TwilioService.new
+  end
+
+  def text
+    gather_reminders
+    send_sms
+  end
+
+  def gather_reminder
+    @reminders = Reminder.new(send_at: @time)
+  end
+
+  def send_messages
+    message_phone_number = reminder.gather_data(@today)
   end
 end
