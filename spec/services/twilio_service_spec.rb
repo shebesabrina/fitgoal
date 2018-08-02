2ebf65ad1250066473bd99812443cdfafb5ab087
Sabrina Robinson <sabrinarob@gmail.com>
2018-08-01 21:00:15 -0600

    Implemented feature test for setting a reminder process

--- spec/services/twilio_service_spec.rb
+++ spec/services/twilio_service_spec.rb
@@ -3,22 +3,14 @@ require 'rails_helper'
 describe TwilioService do
   it 'sends sms messages' do
     service = spy("Twilio::REST::Client")
-    allow("Twilio::REST::Client").to receive(:retrieve).and_return(stripe_customer)
+    
+    allow("Twilio::REST::Client").to receive(:phone_number).and_return(service)
     token = "fake token"
 
-    stub_request(:post, "https://api.twilio.com/2010-04-01/Accounts/AC4ee6b18bbf5482d269109fa8c4c6cbf7/Messages.json").
-      with(
-        body: {"Body"=>"Sup!", "From"=>"18432420990", "To"=>"9703792924"},
-        headers: {
-       'Accept'=>'application/json',
-       'Accept-Charset'=>'utf-8',
-       'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       'Authorization'=>'Basic QUM0ZWU2YjE4YmJmNTQ4MmQyNjkxMDlmYThjNGM2Y2JmNzo4MzgwMjQ2ZWViOTFhOTQwYjNjMDc0MTRmMmQ5ZjUzZg==',
-       'Content-Type'=>'application/x-www-form-urlencoded',
-       'User-Agent'=>'twilio-ruby/5.11.2 (ruby/x86_64-darwin17 2.4.1-p111)'
-        }).
-      to_return(status: 200, body: "Sup!", headers: {})
-      binding.pry
+    post :create, twilio_token: token
+
+    expect(service).to have_received(:body).with(token)
+
     # response = TwilioService.new.send_sms('9703792924', 'Sup!')
     #
     # expect(response.body).to eq("Sent from your Twilio trial account - Sup!")
